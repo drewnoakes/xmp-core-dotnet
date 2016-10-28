4077d937e1375a3153f09691603de5fa9c68fbb7
Drew Noakes <git@drewnoakes.com>
2016-10-28 21:05:43 +0100

    Add some tests for XmpUtil conversions.

---
+++ XmpCore.Tests/XmpUtilsTests.cs
@@ -0,0 +1,35 @@
+﻿using Xunit;
+
+namespace XmpCore.Tests
+{
+    public class XmpUtilsTests
+    {
+        [Fact]
+        public void ConvertToInteger()
+        {
+            Assert.Equal(1, XmpUtils.ConvertToInteger("1"));
+            Assert.Equal(1, XmpUtils.ConvertToInteger("001"));
+            Assert.Equal(1, XmpUtils.ConvertToInteger("0x1"));
+            Assert.Equal(15, XmpUtils.ConvertToInteger("0xF"));
+            Assert.Equal(-1, XmpUtils.ConvertToInteger("-1"));
+            Assert.Equal(123, XmpUtils.ConvertToInteger(" 123 "));
+
+            Assert.Throws<XmpException>(() => XmpUtils.ConvertToInteger(null));
+            Assert.Throws<XmpException>(() => XmpUtils.ConvertToInteger("Foo"));
+        }
+
+        [Fact]
+        public void ConvertToLong()
+        {
+            Assert.Equal(1L, XmpUtils.ConvertToLong("1"));
+            Assert.Equal(1L, XmpUtils.ConvertToLong("001"));
+            Assert.Equal(1L, XmpUtils.ConvertToLong("0x1"));
+            Assert.Equal(15L, XmpUtils.ConvertToLong("0xF"));
+            Assert.Equal(-1L, XmpUtils.ConvertToLong("-1"));
+            Assert.Equal(123L, XmpUtils.ConvertToLong(" 123 "));
+
+            Assert.Throws<XmpException>(() => XmpUtils.ConvertToLong(null));
+            Assert.Throws<XmpException>(() => XmpUtils.ConvertToLong("Foo"));
+        }
+    }
+}
