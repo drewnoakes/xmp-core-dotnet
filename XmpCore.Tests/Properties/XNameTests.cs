43a893bce5a102f58ccba8bbfe0f883f0421fc91
Drew Noakes <git@drewnoakes.com>
2016-10-28 19:30:36 +0100

    Add unit tests exposing XName behaviour.

---
+++ XmpCore.Tests/Properties/XNameTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Xml.Linq;
+using Xunit;
+
+namespace XmpCore.Tests.Properties
+{
+    public class XNameTests
+    {
+        [Fact]
+        public void StringComparisons()
+        {
+            string s = "xmlns";
+            XName n = "xmlns";
+
+            Assert.True(s == n);
+            Assert.True(n == s);
+            Assert.True(s.Equals(n));
+            Assert.False(n.Equals(s));
+        }
+    }
+}
